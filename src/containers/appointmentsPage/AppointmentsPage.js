8d9007e2f2338176d52c3a24883ec877f8676640
Sebastian <github@scherbes.de>
2021-04-03 13:06:36 +0200

    #7 Create Appointments page

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,28 +1,49 @@
 import React, { useState } from "react";
+import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
+import { TileList } from "../../components/tileList/TileList";
 
 export const AppointmentsPage = (props) => {
-  const [ appointments, setAppointments ] = props.appointments;
-  /*
-  Define state variables for 
-  appointment info
-  */
+  const appointments = props.appointments;
+  const contacts = props.contacts;
+  const [ title, setTitle ] = useState('');
+  const [ contact, setContact ] = useState('');
+  const [ date, setDate ] = useState('');
+  const [ time, setTime ] = useState('');
+  
+  const handleSetTitle    = (e) => { setTitle(e.target.value); }
+  const handleSetContact  = (e) => { setContact(e.target.value); }
+  const handleSetDate     = (e) => { setDate(e.target.value); }
+  const handleSetTime     = (e) => { setTime(e.target.value); }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data  
-    */
-   
+    props.addAppointment({title: title, contact: contact, date: date, time: time});
+    setTitle('');
+    setContact('');
+    setDate('');
+    setTime(''); 
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm
+          handleSubmit={handleSubmit}
+          title={title}
+          setTitle={handleSetTitle}
+          contact={contact}
+          setContact={handleSetContact}
+          date={date}
+          setDate={handleSetDate}
+          time={time}
+          setTime={handleSetTime}
+        />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList tileData={appointments} />
       </section>
     </div>
   );
